34d8c7692212ddedf3f3cf59510251818d7e8662
MyeonggyuWoo <woomg427@gmail.com>
2024-08-22 02:26:56 +0900

    3주차 django 개발 - detail페이지 수정 & 로그인/회원가입 기능 구현

--- my_project/settings.py
+++ my_project/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "mysite",
     "pages",
+    "accounts.apps.AccountsConfig",
 ]
 
 MIDDLEWARE = [
@@ -128,3 +129,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
